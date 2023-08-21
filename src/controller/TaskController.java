4dfcf72e4ed069ffebd06daf4ea060c770b9ef09
Mei-2513 <j-uliana9@hotmail.com>
2023-08-21 16:06:30 -0500

    codigo

--- src/controller/TaskController.java
+++ src/controller/TaskController.java
@@ -5,31 +5,107 @@ import java.util.LinkedList;
 import java.util.List;
 
 import model.Task;
-import view.TaskView;
+
+
+import java.text.SimpleDateFormat;
+
+import java.util.Scanner;
 
 public class TaskController {
     private List<Task> tasks;
-    private TaskView view;
+    private Scanner scanner;
 
     public TaskController() {
         tasks = new LinkedList<>();
-        view = new TaskView();
+        scanner = new Scanner(System.in);
+    }
+    
+
+    public void runConsoleApp() {
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("1. Agregar tarea");
+            System.out.println("2. Asignar tarea");
+            System.out.println("3. Mostrar tareas");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consumir la nueva línea después de la entrada numérica
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Ingrese la descripción: ");
+                    String description = scanner.nextLine();
+                    System.out.print("Ingrese la fecha de vencimiento (dd/mm/yyyy): ");
+                    String dueDateStr = scanner.nextLine();
+                    Date dueDate = parseDate(dueDateStr);
+                    addTask(description, dueDate);
+                    break;
+                case 2:
+                    System.out.println("Tareas:");
+                    displayTasks();
+                    System.out.print("Seleccione el índice de la tarea para asignar: ");
+                    int taskIndex = scanner.nextInt();
+                    scanner.nextLine(); // Consumir la nueva línea después de la entrada numérica
+                    System.out.print("Ingrese el nombre del usuario asignado: ");
+                    String userName = scanner.nextLine();
+                    assignTask(taskIndex, userName);
+                    break;
+                case 3:
+                    displayTasks();
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+                    break;
+            }
+        }
     }
 
     public void addTask(String description, Date dueDate) {
         Task task = new Task(description, dueDate);
         tasks.add(task);
+        System.out.println("Tarea agregada con éxito.");
     }
+    
 
     public void assignTask(int taskIndex, String userName) {
         if (taskIndex >= 0 && taskIndex < tasks.size()) {
             Task task = tasks.get(taskIndex);
             task.setAssignedUser(userName);
+            System.out.println("Tarea asignada a " + userName + ".");
+        } else {
+            System.out.println("Índice de tarea no válido.");
         }
     }
 
     public void displayTasks() {
-        view.printTasks(tasks);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        int index = 0;
+        
+        for (Task task : tasks) {
+            System.out.println("Índice: " + index);
+            System.out.println("Descripción: " + task.getDescription());
+            System.out.println("Fecha de vencimiento: " + dateFormat.format(task.getDueDate()));
+            System.out.println("Asignado a: " + task.getAssignedUser());
+            System.out.println("------------");
+            index++;
+        }
+    }
+
+    private Date parseDate(String dateStr) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return dateFormat.parse(dateStr);
+        } catch (Exception e) {
+            System.out.println("Error al analizar la fecha. Utilizando fecha actual.");
+            return new Date();
+        }
     }
 }
+
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,17 +3,29 @@ package main;
 import java.util.Date;
 
 import controller.TaskController;
+import model.Task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+import java.util.Date;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         TaskController controller = new TaskController();
 
-        controller.addTask("Hacer informe", new Date(2023, 8, 31));
-        controller.addTask("Preparar presentación", new Date(2023, 8, 28));
+        controller.addTask("Hacer informe", new Date(2023 - 1900, 9, 1)); // Restamos 1900 al año y restamos 1 al mes
+        controller.addTask("Preparar presentación", new Date(2023 - 1900, 8, 28)); // Restamos 1900 al año y restamos 1 al mes
 
         controller.assignTask(0, "Usuario1");
         controller.assignTask(1, "Usuario2");
 
-        controller.displayTasks();
+        controller.runConsoleApp();
     }
 }
+

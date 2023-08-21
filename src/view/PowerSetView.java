8226d56e5e9f0f81cac57a4e319c61ea80f9c750
Mei-2513 <j-uliana9@hotmail.com>
2023-08-21 17:46:38 -0500

    agregar interfaz

--- src/view/PowerSetView.java
+++ src/view/PowerSetView.java
@@ -3,12 +3,52 @@ package view;
 import java.util.List;
 import java.util.Set;
 
-public class PowerSetView {
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+import model.PowerSetModel;
+
+
+import java.util.*;
+
+public class PowerSetView extends JFrame {
+    private JTextArea textArea;
+
+    public PowerSetView() {
+        setTitle("Power Set Viewer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        add(scrollPane);
+    }
+
     public void displayPowerSet(List<Set<Integer>> powerSet) {
-        System.out.println("Power Set:");
+        textArea.setText("Power Set:\n");
         for (Set<Integer> subset : powerSet) {
-            System.out.println(subset);
+            textArea.append(subset.toString() + "\n");
         }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = new ArrayList<>();
+        inputList.add(1);
+        inputList.add(2);
+        inputList.add(3);
+
+        PowerSetModel model = new PowerSetModel();
+        List<Set<Integer>> powerSet = model.generatePowerSet(inputList);
+
+        SwingUtilities.invokeLater(() -> {
+            PowerSetView view = new PowerSetView();
+            view.displayPowerSet(powerSet);
+            view.setVisible(true);
+        });
     }
 }
 

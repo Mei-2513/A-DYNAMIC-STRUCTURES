85e35a12999096c33f575b445fe8a018d8bd2ba4
Mei-2513 <j-uliana9@hotmail.com>
2023-08-21 17:12:18 -0500

    codigo

--- src/controller/CarritoController.java
+++ src/controller/CarritoController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.Vector;
+
+import model.Articulo;
 import model.CarritoModel;
 import view.CarritoView;
 
@@ -7,22 +10,34 @@ public class CarritoController {
     private CarritoModel carritoModel;
     private CarritoView carritoView;
 
-    public CarritoController(CarritoModel model, CarritoView view) {
-        carritoModel = model;
-        carritoView = view;
+    public CarritoController() {
+        carritoModel = new CarritoModel();
     }
 
-    public void iniciar() {
-        carritoView.actualizarVista(); // Inicializa la vista con los datos del modelo
+    public void setView(CarritoView view) {
+        carritoView = view;
     }
 
     public void agregarArticulo(String nombre, double precio) {
         carritoModel.agregarArticulo(nombre, precio);
-        carritoView.actualizarVista();
     }
 
-   
+    public Vector<Articulo> getCarrito() {
+        return carritoModel.getCarrito();
+    }
+
+    public double getPrecioTotal() {
+        return carritoModel.calcularPrecioTotal();
+    }
+
+    public static void main(String[] args) {
+        CarritoController controller = new CarritoController();
+        CarritoView view = new CarritoView(controller);
+        controller.setView(view);
+        view.setVisible(true);
+    }
 }
+
 
 
 
--- src/view/CarritoView.java
+++ src/view/CarritoView.java
@@ -2,8 +2,8 @@ package view;
 
 import javax.swing.*;
 
+import controller.CarritoController;
 import model.Articulo;
-import model.CarritoModel;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,18 +12,19 @@ import java.util.Vector;
 
 public class CarritoView extends JFrame {
     private JTextArea areaCarrito;
-    private JLabel labelTotal;
-    private CarritoModel carritoModel;
+    private JButton botonAgregar;
+    private JButton botonFinalizar;
+    private CarritoController controller;
 
-    public CarritoView(CarritoModel model) {
-        carritoModel = model;
+    public CarritoView(CarritoController controller) {
+        this.controller = controller;
 
         areaCarrito = new JTextArea(10, 30);
         areaCarrito.setEditable(false);
 
-        labelTotal = new JLabel("Total: ");
+        botonAgregar = new JButton("Agregar Artículo");
+        botonFinalizar = new JButton("Finalizar Compra");
 
-        JButton botonAgregar = new JButton("Agregar Artículo");
         botonAgregar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -31,37 +32,44 @@ public class CarritoView extends JFrame {
                 String precioStr = JOptionPane.showInputDialog("Ingrese el precio del artículo:");
                 double precio = Double.parseDouble(precioStr);
 
-                carritoModel.agregarArticulo(nombre, precio);
-                actualizarVista();
+                controller.agregarArticulo(nombre, precio);
+                actualizarVista(controller.getCarrito());
+            }
+        });
+
+        botonFinalizar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double total = controller.getPrecioTotal();
+                JOptionPane.showMessageDialog(null, "Gracias por su compra.\nPrecio total: $" + total);
+                System.exit(0);
             }
         });
 
         JPanel panel = new JPanel();
         panel.add(new JScrollPane(areaCarrito));
-        panel.add(labelTotal);
         panel.add(botonAgregar);
+        panel.add(botonFinalizar);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Carrito de Compras");
         add(panel);
         pack();
-        setVisible(true);
+        setLocationRelativeTo(null);
     }
 
-    public void actualizarVista() {
-        Vector<Articulo> carrito = carritoModel.getCarrito();
+    public void actualizarVista(Vector<Articulo> carrito) {
         areaCarrito.setText("");
         for (Articulo articulo : carrito) {
             areaCarrito.append(articulo.getNombre() + " - $" + articulo.getPrecio() + "\n");
         }
-        double total = carritoModel.calcularPrecioTotal();
-        labelTotal.setText("Total: $" + total);
     }
 
     public static void main(String[] args) {
-        CarritoModel model = new CarritoModel();
-        CarritoView view = new CarritoView(model);
+        CarritoController controller = new CarritoController();
+        CarritoView view = new CarritoView(controller);
+        controller.setView(view);
+        view.setVisible(true);
     }
 }
-
 
